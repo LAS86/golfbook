13015a40c1c0444d61741839fabe8503bafe6468
Luke <70299112+LAS86@users.noreply.github.com>
2022-11-22 15:30:14 -0600

    add-courses-holes

---
+++ src/main/java/com/example/golfbook/course/course/service/CourseService.java
@@ -0,0 +1,28 @@
+package com.example.golfbook.course.service;
+
+import com.example.golfbook.course.model.Course;
+import com.example.golfbook.course.repository.CourseRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CourseService {
+
+    private final CourseRepository courseRepository;
+
+    public CourseService(CourseRepository courseRepository) {
+        this.courseRepository = courseRepository;
+    }
+
+    public List<Course> getAllByCourseId(){
+        Iterable<Course> courses = this.courseRepository.findAll();
+        List<Course> courseList = new ArrayList<>();
+        courses.forEach(course->{courseList.add(course);});
+        return courseList;
+    }
+
+
+
+}
